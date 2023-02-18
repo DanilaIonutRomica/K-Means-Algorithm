8bfc8d092f72c491863b5c260804543e0b420bf7
danilaionutromica <danilaionut8@gmail.com>
2023-02-18 17:20:47 +0200

    Works TODO:Delete comments and get cluster diameter
    Works TODO:Delete comments and get cluster diameter

--- K_Means.py
+++ K_Means.py
@@ -20,7 +20,7 @@ def calculate_centroid(elements,dimension=2):
         for element in elements:
             sum_dim_i+= element[i]
         if len(elements) == 0:
-            sum_dimensions.append(0)
+            sum_dimensions.append(random.randint(0,100)/random.randint(1,100))
         else:
             sum_dim_i/=len(elements)
             sum_dimensions.append(sum_dim_i)
@@ -48,8 +48,8 @@ def K_Means(dataset, k,dimnesion=2):
     counterul=0
     
     for i in range(0,100):
-    #while (list_of_previous_centroids != centroids) and (counterul !=5) :
-        counterul+=1
+    #while (list_of_previous_centroids != centroids):# and (counterul >=5) :
+        
         #Calculate distance from element to each centroid
         print("se intampla ceva alooooo")
         distance_matrix=[]
@@ -61,39 +61,51 @@ def K_Means(dataset, k,dimnesion=2):
 
         #Assign each element a centroid
         
-        #dataset_cluster=[]
+        dataset_cluster=[]
         indexForClusters=0
         for i in range(0,len(dataset)):
             col_i=column(distance_matrix,i)
             #print(col_i)
             index=col_i.index(min(col_i))
+            #print(col_i)
+            #print(index)
             maxm=min(col_i)
             #print(maxm,index)
             for j in range(0,len(centroids)):
                 
                 if distance_matrix[j][i] == maxm:
-                    print(indexForClusters)
-                    dataset_cluster[indexForClusters]=centroids[index]
+                    #print(indexForClusters)
+                    dataset_cluster.append(index)
                     indexForClusters+=1
-        print(dataset_cluster)
+        #print(dataset_cluster)
         #Recalculate centroids
-        list_of_previous_centroids=centroids
+        list_of_previous_centroids=[]
+        for p in range(0,len(centroids)):
+            list_of_previous_centroids.append(centroids[p])
         #print(list_of_previous_centroids)
         li=0
+        
         for centroid in centroids:
             list_of_elements=[]
             i=0
             for element in dataset_cluster:
-                if element == centroid:
+                #print()
+                if centroids[element] == centroid:
                     
                     list_of_elements.append(dataset[i])
                 i+=1
             centroid=calculate_centroid(list_of_elements)
             centroids[li]=centroid
             li+=1
+        if list_of_previous_centroids == centroids:
+            counterul+=1
+            print(centroids)
+            print(list_of_previous_centroids)  
+        
         #print(centroids)
-    #print(centroids,list_of_previous_centroids)    
-    
+    print(centroids)
+    print(list_of_previous_centroids)    
+    print(dataset_cluster)
     return [dataset,centroids]
                 
 
@@ -101,9 +113,9 @@ def K_Means(dataset, k,dimnesion=2):
 
 
 otherList=[]
-for j in range(0,6):
-    n = random.randint(1,10000)
-    m=random.randint(1,10000)
+for j in range(0,10):
+    n = random.randint(1,100)
+    m=random.randint(1,100)
     randomlist=[m,n]
     otherList.append(randomlist)
 
@@ -124,4 +136,7 @@ for elem in lista[1][:]:
 #print(xx,yy)
 plt.plot(x,y, 'o', color='black')
 plt.plot(xx,yy,'o', color='red')
+
+circle1 = plt.Circle((0, 0), 20, color='r',fill=False)
+plt.gca().add_patch(circle1)
 plt.show()
